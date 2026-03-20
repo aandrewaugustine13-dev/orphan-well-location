c15f3c8b4771152957bf7df20fa7e93fdf78ad74
Andrew Augustine <aandrewaugustine13@gmail.com>
2026-03-20 00:46:20 -0500

    Add files via upload

--- components/Map.tsx
+++ components/Map.tsx
@@ -399,7 +399,7 @@ export default function Map({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; CARTO'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
 
         {showFloodZones && <FloodZoneOverlay />}
